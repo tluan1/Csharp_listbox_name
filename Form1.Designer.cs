40892b4ab8568fd60b6ce4b9ef62e8d755a4a62d
tluan1 <luanthanh253@gmail.com>
2024-06-10 15:03:40 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Csharp_listbox_name
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txttensv = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.lbskq = new System.Windows.Forms.ListBox();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(256, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SÁCH SINH VIÊN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(256, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập họ và tên sinh viên";
+            // 
+            // txttensv
+            // 
+            this.txttensv.Location = new System.Drawing.Point(208, 156);
+            this.txttensv.Name = "txttensv";
+            this.txttensv.Size = new System.Drawing.Size(249, 20);
+            this.txttensv.TabIndex = 2;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(510, 153);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 3;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // lbskq
+            // 
+            this.lbskq.FormattingEnabled = true;
+            this.lbskq.Location = new System.Drawing.Point(208, 191);
+            this.lbskq.Name = "lbskq";
+            this.lbskq.Size = new System.Drawing.Size(249, 160);
+            this.lbskq.TabIndex = 4;
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(604, 153);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 5;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(558, 191);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 6;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.lbskq);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txttensv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txttensv;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.ListBox lbskq;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_listbox_name
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            lbskq.Items.Add(txttensv.Text);
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            lbskq.Items.RemoveAt(lbskq.SelectedIndex);
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            if (lbskq.SelectedIndex < 0) return;
+            lbskq.Items[lbskq.SelectedIndex] = txttensv.Text;
+        }
+        private void lbsketqua_SelectedindexChanged(object sender, EventArgs e)
+        {
+            txttensv.Text = lbskq.SelectedItem.ToString();
+        }
+    }
+}
